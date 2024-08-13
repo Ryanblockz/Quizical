44558a5f77ab5d6790cbb3d3b9cd6fc00f9d3c72
Ryan Siegelberg <siegelbergryan@gmail.com>
2024-08-13 16:36:00 +0200

    Added quiz variables
    Added 3x selectors and their respective state variables.
    
    Users can now choose a quiz category,
    the quiz length and quiz difficulty.
    
    Also hard coded an array filled with quiz category objects, each objects contains an id and string value.

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,182 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import {decode} from "html-entities"
+import { decode } from "html-entities"
 import Quiz from "./components/Quiz"
 
 export default function App() {
+
+  const categories = [
+    { id: 9, name: 'General Knowledge' },
+    { id: 10, name: 'Entertainment: Books' },
+    { id: 11, name: 'Entertainment: Film' },
+    { id: 12, name: 'Entertainment: Music' },
+    { id: 13, name: 'Entertainment: Musicals & Theatres' },
+    { id: 14, name: 'Entertainment: Television' },
+    { id: 15, name: 'Entertainment: Video Games' },
+    { id: 16, name: 'Entertainment: Board Games' },
+    { id: 17, name: 'Science & Nature' },
+    { id: 18, name: 'Science: Computers' },
+    { id: 19, name: 'Science: Mathematics' },
+    { id: 20, name: 'Mythology' },
+    { id: 21, name: 'Sports' },
+    { id: 22, name: 'Geography' },
+    { id: 23, name: 'History' },
+    { id: 24, name: 'Politics' },
+    { id: 25, name: 'Art' },
+    { id: 26, name: 'Celebrities' },
+    { id: 27, name: 'Animals' },
+    { id: 28, name: 'Vehicles' },
+    { id: 29, name: 'Entertainment: Comics' },
+    { id: 30, name: 'Science: Gadgets' },
+    { id: 31, name: 'Entertainment: Japanese Anime & Manga' },
+    { id: 32, name: 'Entertainment: Cartoon & Animations' }
+  ]
+
   const [startQuiz, setStartQuiz] = useState(false)
   const [questions, setQuestions] = useState([])
+  const [selectedAnswers, setSelectedAnswers] = useState([])
+  const [submitted, setSubmitted] = useState(false)
+  const [score, setScore] = useState(0)
+  const [selectedCategory, setSelectedCategory] = useState(categories[0].id)
+  const [quizLength, setQuizLength] = useState(5)
+  const [difficulty, setDifficulty] = useState("easy")
 
-  useEffect(()=>{
+  useEffect(() => {
 
-  const fetchData = async () => {
-    try{
-  const response = await fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-  const data = await response.json()
-  const results = data.results
+    const fetchData = async () => {
 
-  const questions = results.map(result =>{
-  const decodedQuestion = decode(result.question)
-  const correctAnswer = decode(result.correct_answer)
-  const incorrectAnswers = result.incorrect_answers.map(answer => decode(answer))
-  const allAnswers = shuffleArray([correctAnswer, ...incorrectAnswers])
-  
-  return{
-    question: decodedQuestion,
-    correctAnswer,
-    answers: allAnswers
+      try {
+        const response = await fetch(`https://opentdb.com/api.php?amount=${quizLength}&category=${selectedCategory}&type=multiple&difficulty=${difficulty}`)
+        const data = await response.json()
+        const results = data.results
+
+        const questions = results.map(result => {
+          const decodedQuestion = decode(result.question)
+          const correctAnswer = decode(result.correct_answer)
+          const incorrectAnswers = result.incorrect_answers.map(answer => decode(answer))
+          const allAnswers = shuffleArray([correctAnswer, ...incorrectAnswers])
+
+          return {
+            question: decodedQuestion,
+            correctAnswer,
+            answers: allAnswers
+          }
+        })
+
+        setQuestions(questions)
+        setSelectedAnswers(new Array(questions.length).fill(null))
+        setSubmitted(false)
+
+      } catch (error) {
+        console.log("Error fetching data:", error)
+      }
+    }
+
+    if (startQuiz) {
+      fetchData()
+    }
+  }, [startQuiz])
+
+
+  const shuffleArray = (array) => {
+    let shuffled = [...array]
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+    }
+    return shuffled
   }
-})
-
-setQuestions(questions)
-
-  } catch(error) {
-    console.log("Error fetching data:", error)
-  }}
- 
-  if(startQuiz){
-    fetchData()
-   }
-},[startQuiz])
 
 
-const shuffleArray = (array) => {
-  let shuffled = [...array]
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+  const handleAnswerSelect = (index, answer) => {
+    const updatedAnswers = [...selectedAnswers]
+    updatedAnswers[index] = answer
+    setSelectedAnswers(updatedAnswers)
   }
-  return shuffled
-};
 
-  if(!startQuiz){
-  return (
-    <>
-      <h1>Quizzical</h1>
-      <p>Fancy yourself a brain? Click the button to find out</p>
-      <div className="card">
-        <button onClick={() => setStartQuiz(true)}>
-          Start Quiz
-        </button>
-      </div>
-      
-    </>
-  )
-} else {
-  return (
-    <>
-  {questions.map((quiz, index) => (
-          <Quiz 
-            key={index} 
-            question={quiz.question} 
-            answers={quiz.answers} 
-            correctAnswer={quiz.correctAnswer} 
+  const calculateScore = () => {
+    let score = 0;
+    selectedAnswers.forEach((answer, index) => {
+      if (answer === questions[index].correctAnswer) {
+        score++
+      }
+    })
+    return score
+  }
+
+  const handleSubmit = () => {
+    if (!submitted) {
+      const calculatedScore = calculateScore()
+      setScore(calculatedScore)
+      setSubmitted(true)
+    } else {
+      setStartQuiz(false)
+      setQuestions([])
+      setSelectedAnswers([])
+      setScore(0)
+    }
+  }
+
+  if (!startQuiz) {
+    return (
+      <>
+        <h1>Quizzical</h1>
+        <div className="selector-group">
+          <label htmlFor="category-select">Select Category:</label>
+          <select id="category-select" onChange={(e) => setSelectedCategory(e.target.value)} value={selectedCategory}>
+            {categories.map(category => (
+              <option key={category.id} value={category.id}>{category.name}</option>
+            ))}
+          </select>
+        </div>
+
+        <div className="selector-group">
+          <label htmlFor="quiz-length-select">Select Quiz Length:</label>
+          <select id="quiz-length-select" onChange={(e) => setQuizLength(e.target.value)} value={quizLength}>
+            {Array.from({ length: 10 }, (_, i) => i + 1).map(length => (
+              <option key={length} value={length}>{length}</option>
+            ))}
+          </select>
+        </div>
+
+        <div className="selector-group">
+          <label htmlFor="difficulty-select">Select Difficulty:</label>
+          <select id="difficulty-select" onChange={(e) => setDifficulty(e.target.value)} value={difficulty}>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+        </div>
+        <p>Fancy yourself a brain? Click the button to find out</p>
+        <div className="card">
+          <button onClick={() => setStartQuiz(true)}>
+            Start Quiz
+          </button>
+        </div>
+
+      </>
+    )
+  } else {
+    return (
+      <>
+        {questions.map((quiz, index) => (
+          <Quiz
+            key={index}
+            question={quiz.question}
+            answers={quiz.answers}
+            correctAnswer={quiz.correctAnswer}
+            selectedAnswer={selectedAnswers[index]}
+            onAnswerSelect={(answer) => handleAnswerSelect(index, answer)}
+            submitted={submitted}
           />
         ))}
-    </>
-  )
-}
+        {submitted && <p>You scored {score}/{quizLength}</p>}
+        <div>
+          <button onClick={handleSubmit}>{submitted ? 'Play New Game' : 'Submit Quiz'}</button>
+        </div>
+      </>
+    )
+  }
 }
 
 
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,34 @@
 import { useState, useEffect } from 'react'
-import {decode} from "html-entities"
+import { decode } from "html-entities"
 
+export default function Quiz({ question, answers, selectedAnswer, onAnswerSelect, correctAnswer, submitted }) {
+  const getAnswerClass = (answer) => {
+    if (submitted) {
+      if (answer === correctAnswer) return 'correct'
+      if (answer === selectedAnswer) return 'incorrect'
+      return 'unselected';
+    } else {
+      if (answer === selectedAnswer) return 'selected'
+      return ''
+    }
+  };
 
-export default function Quiz(props) {
-    const question = props.question
-    const answers = props.answers
-    const correctAnswer = props.correctAnswer
-    
-    return (
+  return (
+    <div className="quiz">
+      <h2>{question}</h2>
+      <ul className="quizAnswersContainer">
+        {answers.map((answer, index) => (
+          <li
+            key={index}
+            className={`answers ${getAnswerClass(answer)}`}
+            onClick={() => onAnswerSelect(answer)}
+          >
+            {answer}
+          </li>
+        ))}
+      </ul>
+      <hr style={{ width: "100%" }} />
+    </div>
 
-      <div className="quiz">
-        <h2>{question}</h2>
-        <ul>
-          {answers.map((answer, index) => (
-            <li key={index}>{answer}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+  );
+}
